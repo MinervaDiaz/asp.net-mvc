744cda508a2b20f2fad775b1193538c87274b627
MinervaD <minervadiago@gmail.com>
2023-08-15 12:41:57 -0600

    Add edit method in CamionController & add edit icon

--- Controllers/CamionesController.cs
+++ Controllers/CamionesController.cs
@@ -99,5 +99,64 @@ namespace TransportesMVC.Controllers
             //asignamos la propiedad notification y el mensaje
             TempData["notification"]=msg;
         }
+
+
+        //EDITAR
+        public ActionResult Editar_Camion(int id)
+        {
+            //llamamos al contexto, voy a trabajar con el contexto
+            camiones camiones = new camiones();
+            using (TransportesEntities db = new TransportesEntities())
+            {
+                camiones = db.camiones.Where(x => x.id_camion == id).FirstOrDefault();
+            }
+            ViewBag.Title = "Editar Camion n°"+camiones.id_camion;
+            return View(camiones);
+        }
+        [HttpPost]
+        //Polimorfismo de los métodos de arriba y abajo, enel método de abajo regresamos la peticion de POST, arriba solo es GET
+        public ActionResult Editar_Camion(NuevoCamion model)
+        {
+            try
+            {
+                //validamos si el modelo es válido: esto es un helper para validar datos
+                //modelstate es por parte de razor
+                if (ModelState.IsValid)
+                {
+                    //entro a mi contexto de la base de datos
+                    using (TransportesEntities db = new TransportesEntities())
+                    {
+                        var camion = new camiones();
+                        camion.id_camion = model.id_camion;
+                        camion.matricula = model.matricula;
+                        camion.tipo_camion = model.tipo_camion;
+                        camion.marca = model.marca;
+                        camion.modelo = model.modelo;
+                        camion.capacidad = model.capacidad;
+                        camion.kilometraje = model.kilometraje;
+                        camion.urlFoto = model.urlFoto;
+                        camion.disponibilidad = model.disponibilidad;
+
+                        //dentro del mi contexto, en el contexto de camiones se guardan los datos
+                        db.Entry(camion).State = System.Data.Entity.EntityState.Modified;
+                        //db.camiones.Add(camion);
+                        db.SaveChanges();
+                        Alert("Registro guardado con éxito", NotificationType.success);
+                    }
+
+                    //si lo anterior se hizo con éxito regreso a la vista de Camiones
+                    return Redirect("~/Camiones");
+                }
+                //SI REDIRECCIONO AL VIEW MODEL ES PORQUE FALLÓ UNA VALIDACIÓN
+                Alert("Verificar la información", NotificationType.warning);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                //throw new Exception(ex.Message);
+                Alert("Ha ocurrido un error: " + ex.Message, NotificationType.error);
+                return View(model);
+            }
+        }
     }
 }
